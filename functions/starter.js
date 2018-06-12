65121bdaf65fba663122125f58d9eaa2d89edd07
bdemers1055 <bdemers1055@gmail.com>
2018-06-11 20:39:53 -0400

    fun with booleans

--- functions/starter.js
+++ functions/starter.js
@@ -20,6 +20,28 @@ function length(x) {
     return x.length;
 }
 
+/**
+ * {param} a - number of the small side a triangle
+ * {param} b - number of the middle side of a triangle
+ * (param} c - number of a hypotenuse
+ * @return boolean true if a^2 + b^2 === c^2
+ */
+
+function triple(a, b, c ) {
+    return a * a + b * b === c * c;
+}
+
+/**
+ * @param x first number
+ * @param y second number
+ * @return the difference of x and y (x - y)
+ */
+
+function sub(x, y) {
+    return x - y;
+}
+
+
 const result = concat("yayyy", "wooo!!!");
 console.log(result);
 const result2 = concat(10, 3);
@@ -36,3 +58,11 @@ console.log(sum);
 
 const lengthOfCheese = length('cheese');
 console.log(lengthOfCheese);
+
+
+console.log("Is 4 ,5 ,5 a triple?", triple(3, 4, 5));
+console.log("Is 4 ,5 ,5 a triple?", triple(5, 9, 16));
+
+// sub test cases
+console.log('sub(3,4) should be -1', sub(3, 4));
+console.log('sub(10,4) should be 6', sub(10, 4));
