6aa0c6997fbd3b048abcd7afae7eef7966f20d24
bdemers1055 <bdemers1055@gmail.com>
2018-06-14 19:36:42 -0400

    used a for loop

---
+++ loops/starter.js
@@ -0,0 +1,22 @@
+// For loops are good when you know how many
+// While loop are good if you need to do something 'until a condition changes'
+
+// Exercise: I want to print the numbers 1 - 10 with each number on a new line.
+// without a loop
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+// WITH A FOR LOOP
+//  start     // stop   step
+for(let i = 1; i < 11; i += 1){ // could be 1<=10 also
+console.log(i);
+}
+
